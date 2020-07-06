bdbf2ee0321169d47c02e3adc50829a579fb577a
Brian Hyun <jso.venn@gmail.com>
2020-07-06 00:10:25 -0700

    Finish testimonial carousel animation

---
+++ scripts/index.js
@@ -0,0 +1,23 @@
+$(".test.first").click(function() {
+  $(".testimonials-container").animate({
+    left: "450px"
+  }, 1000);
+});
+
+$(".test.second").click(function() {
+  $(".testimonials-container").animate({
+    left: "-130px"
+  }, 1000);
+});
+
+$(".test.third").click(function() {
+  $(".testimonials-container").animate({
+    left: "-710px"
+  }, 1000);
+});
+
+$(".test.fourth").click(function() {
+  $(".testimonials-container").animate({
+    left: "-1290px"
+  }, 1000);
+});
